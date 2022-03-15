791f7f48b2d06c821dc69ac3f2c3c42e84ac3176
bdp1312 <bdp1312@gmail.com>
2022-03-15 11:52:23 -0500

    updating changes

--- WordleSolver.py
+++ WordleSolver.py
@@ -1,26 +1,38 @@
 #!/usr/bin/python3
+'''
+portions of this code were copied from https://gist.github.com/magnusviri/a93b5ab02663f30eac176ca49b0250e3
+ty m8!
+'''
 
 import sys
-import os
+from os.path import exists
+from os.path import expanduser
 
 #full file name
-wordFilePath = os.path.expanduser("~/Wordle-Solver_bdp1312/.words.txt")
+wordFilePath = expanduser("~/Wordle_Solver/.words.txt")
 
 
 #open file
+if exists(wordFilePath):
+    fh = open(wordFilePath, 'r')
+    print("File Open")
+else:
+    print("File Not Found.")
 
 
 #add each word to list
+masterList = []
+
 
 #while there is more than 1 possible word:
 
-#analize letter frequency
+    #analize letter frequency
 
-#sort words by sum of letter frequency
+    #sort words by sum of letter frequency
 
-#select word with greatest score
+    #select word with greatest score
 
-#get updated word information from guess
+    #get updated word information from guess
 
 
 
